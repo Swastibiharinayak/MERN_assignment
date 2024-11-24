f7463600bb9062453b7d6cedf7bd1219f1134e5b
Swastibiharinayak <nayakswastibihari@gmail.com>
2024-11-24 05:00:54 +0000

    Assignment_1 completed

---
+++ Week_3/Assignment_1.js
@@ -0,0 +1,15 @@
+let str = "Hello World";
+let num = 17;
+let bool = true;
+let undef;
+let obj = { name: "Swasti", age: 23 };
+let degree = null;
+
+
+
+console.log(`String: "${str}" -> Type: ${typeof str}`);
+console.log(`Number: ${num} -> Type: ${typeof num}`);
+console.log(`Boolean: ${bool} -> Type: ${typeof bool}`);
+console.log(`Undefined: ${undef} -> Type: ${typeof undef}`);
+console.log(`Object: ${JSON.stringify(obj)} -> Type: ${typeof obj}`);
+console.log(`Null: "${degree}" -> Type: ${typeof degree}`);
