8a29dfaf0c53f1f80e18fd35fcfc7f4ed7d80d1c
Swastibiharinayak <nayakswastibihari@gmail.com>
2024-11-24 16:23:52 +0530

    Create Assignment_2.js
    This the javascript file

---
+++ Week_3/Assignment_2/Assignment_2.js
@@ -0,0 +1,7 @@
+let num1 = parseInt(prompt("Enter the first number:"));
+let num2 = parseInt(prompt("Enter the second number:"));
+
+alert(`Addition: ${num1 + num2}`);
+alert(`Subtraction: ${num1 - num2}`);
+alert(`Multiplication: ${num1 * num2}`);
+alert(`Division: ${num1 / num2}`);
