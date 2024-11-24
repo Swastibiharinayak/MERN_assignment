b700afc8ca847cd4693c8f044896bb1062fdaac9
Swastibiharinayak <nayakswastibihari@gmail.com>
2024-11-24 16:36:10 +0530

    Create Assignment_4.js
    This js file shows a Grading System

---
+++ Week_3/Assignment_4/Assignment_4.js
@@ -0,0 +1,11 @@
+let marks = parseInt(prompt("Enter marks:"));
+
+if (marks >= 90) {
+    alert("Grade: A");
+} else if (marks >= 75) {
+    alert("Grade: B");
+} else if (marks >= 50) {
+    alert("Grade: C");
+} else {
+    alert("Fail");
+}
