e184134bc23d3a29b677290935f7b8bd011b5eec
Swastibiharinayak <nayakswastibihari@gmail.com>
2025-01-13 21:19:53 +0530

    Assignment_9 Updated

--- Week_3/Assignment_9/Assignment_9.js
+++ Week_3/Assignment_9/Assignment_9.js
@@ -1,8 +1,15 @@
 let arr = [];
+
 for (let i = 0; i < 5; i++) {
     arr.push(parseInt(prompt(`Enter number ${i + 1}:`)));
 }
-let reversed = arr.reverse();
 
+// let reversed = arr.reverse();
+
+let reversed = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    reversed[reversed.length] = arr[i];
+}
 
 alert(`Reversed array: ${reversed}`);
