d41aa10ef264ace4adb387e1ddb09a31b72a2604
Swastibiharinayak <nayakswastibihari@gmail.com>
2025-01-13 19:02:38 +0530

    Assignment_8 updated

--- Week_3/Assignment_8/Assignment_8.js
+++ Week_3/Assignment_8/Assignment_8.js
@@ -1,4 +1,10 @@
 let str = prompt("Enter a string:");
 
+// alert(`Length of the string: ${str.length}`);
 
-alert(`Length of the string: ${str.length}`);
+let length = 0;
+for (let i = 0; str[i] !== undefined; i++) {
+    length++;
+}
+
+alert(`Length of the string: ${length}`);
