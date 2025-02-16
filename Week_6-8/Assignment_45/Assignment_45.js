b2952c9b909dd409355c696b2378e741ce2edd37
Swastibiharinayak <nayakswastibihari@gmail.com>
2025-02-16 20:41:56 +0530

    Create Assignment_45.js

---
+++ Week_6-8/Assignment_45/Assignment_45.js
@@ -0,0 +1,15 @@
+// 45.Write a JavaScript program that checks two integer values and returns true if either one is 15 or if their sum or difference is 15.
+
+function fifteen(x,y) {
+    if (x == 15 || y == 15 || x + y == 15 || x - y == 15 || y - x == 15) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(fifteen(15, 9));
+console.log(fifteen(9, 15));
+console.log(fifteen(7, 8));
+console.log(fifteen(25, 10));
+console.log(fifteen(5, 5));
