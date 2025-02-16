9bf6aca72530d37ad68427641af65210d167695c
Swastibiharinayak <nayakswastibihari@gmail.com>
2025-02-16 20:44:10 +0530

    Completed Assignment_48

---
+++ Week_6-8/Assignment_48/Assignment_48.js
@@ -0,0 +1,8 @@
+// 48. Write a JavaScript program to reverse a given string.
+
+function reverseStr(str) {
+    return (str.split('').reverse().join(''))
+}
+
+console.log(reverseStr('hello'))
+console.log(reverseStr('Swasti'))
