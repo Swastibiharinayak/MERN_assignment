a27b88bac3b7db01987e4545f831d8cd8fc16789
Swastibiharinayak <nayakswastibihari@gmail.com>
2025-02-16 20:53:14 +0530

    Completed Assignment_90

---
+++ Week_6-8/Assignment_90/Assignment_90.js
@@ -0,0 +1,14 @@
+// 90. Find kth Greatest Element in Array
+
+// Write a JavaScript program to find the kth greatest element in a given array of integers.  
+
+
+function kthGreatest(arr, k) {
+    arr.sort((a, b) => b - a);
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+        if (k == i+1) {
+            console.log(`${k}'th greatest element is ${arr[i]}`);
+        }
+    }
+}
