294b316b48695dc0a97ca4e254b34e9174c68f7f
Swastibiharinayak <nayakswastibihari@gmail.com>
2025-02-17 20:02:52 +0530

    Completed Assignment_51

---
+++ Week_6-8/Assignment_51/Assignment_51.js
@@ -0,0 +1,11 @@
+// 50. Capitalize First Letter of Each Word in String
+
+// Write a JavaScript program to capitalize the first letter of each word in a given string.
+
+let num= 75;
+function num_To_time(num){
+    let hours = Math.floor(num/60);
+    let minutes = num%60;
+    return hours + ":" + minutes;
+}
+console.log(num_To_time(num));
